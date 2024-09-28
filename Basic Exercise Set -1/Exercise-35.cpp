2a10ff896e195839cbb4ce1ced8e8e29a700bb75
Gaurav Dixit <dgaurav300@gmail.com>
2024-09-28 15:46:56 +0530

    28th Sept 2024 Last Update

---
+++ Basic Exercise Set -1/Exercise-35.cpp
@@ -0,0 +1,16 @@
+#include <iostream> // Including the input-output stream header file
+
+using namespace std; // Using the standard namespace
+
+int main() // Start of the main function
+{
+    cout << "\n\n Compute the specified expressions and print the output:\n"; // Outputting a message indicating the purpose of the program
+    cout << "------------------------------------------------------------\n"; // Outputting a separator line
+
+    // Calculating and outputting the result of the given expression
+    cout << " Result of the expression "
+<< "(25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) is : " // Outputting the expression
+<< (25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5) << "\n"; // Calculating and outputting the result of the expression
+
+    return 0; // Returning 0 to indicate successful program execution
+};
